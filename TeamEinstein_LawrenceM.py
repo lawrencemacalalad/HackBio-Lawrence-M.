c4935b9979e0d66bca4855d49e3709dbf9df543b
lawrencemacalalad <69102093+lawrencemacalalad@users.noreply.github.com>
2020-08-02 22:33:08 +0800

    Add files via upload

---
+++ TeamEinstein_LawrenceM.py
@@ -0,0 +1,9 @@
+
+print("Name: Lawrence S. Macalalad")
+print("Email: lawrencesevillamacalalad@gmail.com")
+print("Language: Python")
+print("Biostack: Genomics")
+print("Slack Username: @Lawrence M.")
+
+
+input("Press any key to exit")
